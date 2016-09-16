a3d6e8271dd85675a7753a81c4bce5dbf1333ec8
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2016-09-16 13:52:32 -0500

    Wrote some more tests

--- src/test/java/com/dustinroepsch/ctci/datastructures/LinkedListDRRTest.java
+++ src/test/java/com/dustinroepsch/ctci/datastructures/LinkedListDRRTest.java
@@ -24,4 +24,32 @@ public class LinkedListDRRTest {
         }
         assertEquals(5, list.size());
     }
+
+    @Test
+    public void testSize5ElementsAfterRemoval() {
+        LinkedListDRR<Integer> list = new LinkedListDRR<Integer>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < 5; i++) {
+            list.remove(0);
+        }
+        assertEquals(5, list.size());
+    }
+
+    @Test
+    public void testAdd() {
+        LinkedListDRR<String> list = new LinkedListDRR<String>();
+        list.add("Hello");
+        assertEquals("Hello", list.get(0));
+    }
+
+    @Test
+    public void testAddMultiple() {
+        LinkedListDRR<String> list = new LinkedListDRR<String>();
+        list.add("Hello");
+        list.add("World");
+        assertTrue(list.get(0).equals("Hello") && list.get(1).equals("World"));
+    }
+
 }
