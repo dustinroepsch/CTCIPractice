ce102cf4c3a57fc8538999b8fab2d7d296aba1f6
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2016-09-15 23:10:31 -0500

    LinkedListDRR Constructor

--- src/LinkedListDRR.java
+++ src/LinkedListDRR.java
@@ -1,7 +1,14 @@
-package PACKAGE_NAME;
+import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Dustin Ryan-Roepsch on 9/15/2016.
  */
-public class LinkedListDRR {
+public class LinkedListDRR<E> {
+    Node head;
+
+    public LinkedListDRR() {
+        head = null;
+    }
 }
