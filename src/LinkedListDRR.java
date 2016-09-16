105f1a292ca9720754740d9adbc177b0a2f887dc
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2016-09-15 23:13:31 -0500

    Elements in linked list must be comparable

--- src/LinkedListDRR.java
+++ src/LinkedListDRR.java
@@ -5,10 +5,11 @@ import java.util.List;
 /**
  * Created by Dustin Ryan-Roepsch on 9/15/2016.
  */
-public class LinkedListDRR<E> {
-    Node head;
+public class LinkedListDRR<E extends Comparable<E>> {
+    Node<E> head;
 
     public LinkedListDRR() {
         head = null;
     }
-}
+
+    public }
