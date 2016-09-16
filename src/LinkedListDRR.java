bd28b2e31aedc19fa85b061d0c50d5f34b4ccf06
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2016-09-15 23:22:40 -0500

    Wrote LinkList add and toString

--- src/LinkedListDRR.java
+++ src/LinkedListDRR.java
@@ -7,4 +7,33 @@ public class LinkedListDRR<E extends Comparable<E>> {
     public LinkedListDRR() {
         head = null;
     }
+
+    public void add(E element) {
+        if (head == null) {
+            head = new Node<E>(element);
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            Node<E> elementNode = new Node<E>(element);
+            current.next = elementNode;
+            elementNode.prev = current;
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (head == null) return "[]";
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        Node<E> current = head;
+        while (current.next != null) {
+            sb.append(current.value.toString());
+            sb.append(',');
+        }
+        sb.append(current);
+        sb.append(']');
+        return sb.toString();
+    }
 }
