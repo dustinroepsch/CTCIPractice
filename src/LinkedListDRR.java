f28a673dec3ae220e4ed17774cce664421d984ea
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2016-09-16 13:04:32 -0500

    Finished Linked List implementation (I think)

--- src/LinkedListDRR.java
+++ src/LinkedListDRR.java
@@ -2,10 +2,12 @@
  * Created by Dustin Ryan-Roepsch on 9/15/2016.
  */
 public class LinkedListDRR<E extends Comparable<E>> {
-    Node<E> head;
+    private Node<E> head;
+    private int size;
 
     public LinkedListDRR() {
         head = null;
+        size = 0;
     }
 
     public void add(E element) {
@@ -20,6 +22,44 @@ public class LinkedListDRR<E extends Comparable<E>> {
             current.next = elementNode;
             elementNode.prev = current;
         }
+        size++;
+    }
+
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
+    }
+
+    public void remove(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        if (current.prev == null && current.next == null) {
+            head = null;
+        } else if (current.prev == null) {
+            head = current.next;
+            head.prev = null;
+        } else if (current.next == null) {
+            current.prev.next = null;
+        } else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        size--;
+    }
+
+    public int size() {
+        return size;
     }
 
     @Override
